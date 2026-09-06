60be389c280468e09dfd2b659f914108e3dfd232
glanemaria969-debug <glanemaria969@gmail.com>
2026-09-07 01:45:01 +0300

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,43 @@
+print("Hello,Glane!")
+name="Glane"
+age=21
+country="Uganda"
+space=3
+print(name)
+print(age)
+print(country)
+print(space)
+name=input("Enter your name:Glane")
+print('Hello,Glane!')
+
+        
+
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+name="Glane"
+age=20
+print("My name is,Glane")
+favorite_nummber=7
+print('My favorite_number is,favorite_number')
+name=input("What is your name?")
+age=input("How old are you")
+print("Hello")
+name=input("What is your name?Glane")
+print("Hello,Glane")
+
+
+name=input("What is your name?,Glane")
+age=input("What is your age?21")
+print("My name is Glane ")
+print("I am 20 years of age")
+name=input("What is your name?Deborah")
+age=input("What is your age?21")
+favorite_colour=input("What is your favorite_colour?Blue")
+print("My name is,Deborah")
+print("I am 21 years old")
+print("My favorite_colour is Blue
